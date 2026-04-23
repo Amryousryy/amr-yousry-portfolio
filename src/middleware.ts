d6c33df941847e39fea359cc3c4fa208e159400e
Amr Yousry <amr@example.com>
2026-04-23 15:37:46 +0200

    Update middleware matcher config

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,3 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 import { routing } from './routing';
  
 export default createMiddleware(routing);
+
+export const config = {
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+};
